090f879c209704281db96688422d4dbcde7c76ef
victormartin <victormartin@github.com>
2014-11-24 14:56:54 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+GAE user API exercise
---
+++ user_exercise.py
@@ -0,0 +1,19 @@
+from google.appengine.api import users
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        user = users.get_current_user()
+
+        if not user:
+            self.redirect(users.create_login_url(self.request.uri))
+        else:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.write('Hello ' + user.nickname())
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
